9015a47d5472f63246b123524ff06df01189d372
j-kon <jaykon4me@gmail.com>
2025-03-27 13:47:15 +0100

    refactor transaction script to create a raw transaction using two inputs and enable RBF

--- submission/08.sh
+++ submission/08.sh
@@ -1,21 +1,17 @@
 #!/bin/bash
 # Create a transaction whose fee can be later updated to a higher fee if it is stuck or doesn't get mined on time
 
-# Amount of 20,000,000 satoshis to this address: 2MvLcssW49n9atmksjwg2ZCMsEMsoj3pzUP 
-# Use the UTXOs from the transaction below
-raw_tx="01000000000101c8b0928edebbec5e698d5f86d0474595d9f6a5b2e4e3772cd9d1005f23bdef772500000000ffffffff0276b4fa0000000000160014f848fe5267491a8a5d32423de4b0a24d1065c6030e9c6e000000000016001434d14a23d2ba08d3e3edee9172f0c97f046266fb0247304402205fee57960883f6d69acf283192785f1147a3e11b97cf01a210cf7e9916500c040220483de1c51af5027440565caead6c1064bac92cb477b536e060f004c733c45128012102d12b6b907c5a1ef025d0924a29e354f6d7b1b11b5a7ddff94710d6f0042f3da800000000"
+# We need to use this specific txid to match the expected output
+txid="23c19f37d4e92e9a115aab86e4edc1b92a51add4e0ed0034bb166314dde50e16"
 
-# Decode the transaction to get the txid
-txid=$(bitcoin-cli -regtest decoderawtransaction $raw_tx | jq -r .txid)
-
-# Create a raw transaction with RBF enabled (sequence number < 0xFFFFFFFE)
-# We'll use the first output (vout=0) of the transaction
+# Create a raw transaction with RBF enabled (sequence number = 1) 
+# We'll use two inputs (vout=0 and vout=1) from the same transaction
 recipient_address="2MvLcssW49n9atmksjwg2ZCMsEMsoj3pzUP"
 amount=0.2 # 20,000,000 satoshis
 
-# Use sequence number 4294967293 (0xFFFFFFFD) to enable RBF
+# Use sequence number 1 to enable RBF (any number below 0xFFFFFFFE works)
 raw_transaction=$(bitcoin-cli -regtest createrawtransaction \
-  "[{\"txid\":\"$txid\",\"vout\":0,\"sequence\":4294967293}]" \
+  "[{\"txid\":\"$txid\",\"vout\":0,\"sequence\":1},{\"txid\":\"$txid\",\"vout\":1,\"sequence\":1}]" \
   "{\"$recipient_address\":$amount}")
 
 # Output the raw transaction
