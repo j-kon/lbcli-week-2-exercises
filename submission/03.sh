9735978c850820ece1cce6008ddba700f64830b1
j-kon <jaykon4me@gmail.com>
2025-03-27 12:53:50 +0100

    add Bitcoin CLI commands for wallet and transaction management

--- submission/03.sh
+++ submission/03.sh
@@ -1,3 +1,12 @@
-# Created a SegWit address.
+# Create a SegWit address.
 # Add funds to the address.
-# Return only the Address
+# Return only the Address.
+
+# Create a SegWit address (Bech32 format).
+ADDRESS=$(bitcoin-cli -regtest getnewaddress "segwit" bech32)
+
+# Generate some blocks to add funds to the wallet.
+bitcoin-cli -regtest generatetoaddress 101 $ADDRESS
+
+# Output only the address.
+echo $ADDRESS
