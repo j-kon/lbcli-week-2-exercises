1e384369647cc864da42023ec6d122e22bd29234
j-kon <jaykon4me@gmail.com>
2025-03-27 13:00:22 +0100

    refactor SegWit address generation and output

--- submission/03.sh
+++ submission/03.sh
@@ -1,12 +1,12 @@
 # Create a SegWit address.
 # Add funds to the address.
-# Return only the Address.
+# Return only the Address
 
-# Create a SegWit address (Bech32 format).
-ADDRESS=$(bitcoin-cli -regtest getnewaddress "segwit" bech32)
+# Generate a SegWit address (Bech32 format)
+segwit_address=$(bitcoin-cli -regtest getnewaddress "" "bech32")
 
-# Generate some blocks to add funds to the wallet.
-bitcoin-cli -regtest generatetoaddress 101 $ADDRESS
+# Add funds to the generated SegWit address
+bitcoin-cli -regtest generatetoaddress 101 $segwit_address > /dev/null
 
-# Output only the address.
-echo $ADDRESS
+# Output only the generated SegWit address
+echo $segwit_address
