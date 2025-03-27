741064f83cadc086543004f5cef8de1bed26a5c7
j-kon <jaykon4me@gmail.com>
2025-03-27 13:38:06 +0100

    refactor transaction script to create a raw transaction using two inputs from a specific txid

--- submission/07.sh
+++ submission/07.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 
-# Decode the raw transaction to extract the UTXO details
-raw_tx="01000000000101c8b0928edebbec5e698d5f86d0474595d9f6a5b2e4e3772cd9d1005f23bdef772500000000ffffffff0276b4fa0000000000160014f848fe5267491a8a5d32423de4b0a24d1065c6030e9c6e000000000016001434d14a23d2ba08d3e3edee9172f0c97f046266fb0247304402205fee57960883f6d69acf283192785f1147a3e11b97cf01a210cf7e9916500c040220483de1c51af5027440565caead6c1064bac92cb477b536e060f004c733c45128012102d12b6b907c5a1ef025d0924a29e354f6d7b1b11b5a7ddff94710d6f0042f3da800000000"
+# We need to use this specific txid and create a transaction with two inputs
+txid="23c19f37d4e92e9a115aab86e4edc1b92a51add4e0ed0034bb166314dde50e16"
 
-# Extract UTXO details
-utxo_txid=$(bitcoin-cli -regtest decoderawtransaction $raw_tx | jq -r '.txid')
-utxo_vout=$(bitcoin-cli -regtest decoderawtransaction $raw_tx | jq -r '.vout[0].n')
-
-# Create a raw transaction
+# Create a raw transaction with two inputs (using index 0 and 1)
 recipient_address="2MvLcssW49n9atmksjwg2ZCMsEMsoj3pzUP"
 amount=0.2 # 20,000,000 satoshis = 0.2 BTC
+
+# Notice we're using two inputs from the same transaction
 raw_transaction=$(bitcoin-cli -regtest createrawtransaction \
-  "[{\"txid\":\"$utxo_txid\",\"vout\":$utxo_vout}]" \
+  "[{\"txid\":\"$txid\",\"vout\":0},{\"txid\":\"$txid\",\"vout\":1}]" \
   "{\"$recipient_address\":$amount}")
 
 # Output the raw transaction
